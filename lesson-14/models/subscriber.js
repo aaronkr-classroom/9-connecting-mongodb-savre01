b17eaf0dcb5258a3f831ceba0c12c0fd6b7b53b1
ouhyeon <163230719+savre01@users.noreply.github.com>
2024-05-01 06:17:35 +0000

    lesson13,14

--- lesson-14/models/subscriber.js
+++ lesson-14/models/subscriber.js
@@ -5,6 +5,16 @@
  * @TODO: Listing 14.3 (p. 206)
  * 스키마 정의
  */
+const mongoose = require("mongoose"),
+    SubscriberSchema = mongoose.Schema({
+        name: String,
+        email: String,
+        phone: Number,
+        newsletter: Boolean
+    });
 
-
-// Subscriber 모델 생성
+// Subscriber 모델 내보내기
+module.exports = mongoose.model(
+    "Subscriber",
+    SubscriberSchema
+);
